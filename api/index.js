bc2d75e342e947057fec638afdd22e7e9fba4fd0
Achal L <achalsl2588@gmail.com>
2021-08-21 17:24:44 +0530

    update user route and verify user token module added, added jwt verification for auth login

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ const app = express()
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 const authRoute = require('./routes/auth')
+const userRoute = require('./routes/users')
 
 dotenv.config()
 
@@ -18,6 +19,7 @@ mongoose
 app.use(express.json())
 
 app.use('/api/auth', authRoute)
+app.use('/api/users', userRoute)
 
 app.listen(8800, () => {
     console.log('backend server is running')
--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,12 +1,13 @@
 const router = require('express').Router()
 const User = require('../models/User')
 const CryptoJS = require("crypto-js");
+const jwt = require('jsonwebtoken')
 
 // REGISTER
 router.post('/register', async (req, res) => {
     const newUser = new User({
         username: req.body.username,
-        password: CryptoJS.AES.encrypt(req.body.password, process.env.SECRET_KEY).toString(),
+        password: CryptoJS.AES.encrypt(req.body.password, process.env.CRYPT_SECRET_KEY).toString(),
         email: req.body.email,
     })
 
@@ -22,21 +23,25 @@ router.post('/register', async (req, res) => {
 router.post('/login', async (req, res) => {
     try {
         const user = await User.findOne({email: req.body.email})
-
         if(!user) {
             return res.status(401).json('Incorrect username or user does not exist')
         }
-        
-        const bytes  = CryptoJS.AES.decrypt(user.password, process.env.SECRET_KEY);
+        const bytes  = CryptoJS.AES.decrypt(user.password, process.env.CRYPT_SECRET_KEY);
         const originalPassword = bytes.toString(CryptoJS.enc.Utf8);
 
         if(originalPassword !== req.body.password) {
             return res.status(401).json('Incorrect password!')
         }
+
+        const accessToken = jwt.sign(
+            { id: user._id, isAdmin: user.isAdmin }, 
+            process.env.JWT_SECRET_KEY,
+            { expiresIn: '5d' }
+        )
         
         const { password, ...info } = user._doc
 
-        return res.status(200).json(info)
+        return res.status(200).json({...info, accessToken})
     } catch (err) {
         return res.status(500).json(err)
     }
